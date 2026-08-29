b8e7419d08198e9da66278cd872fdd9434bb8caf
tranthomas3000-prog <tranthomas3000@gmail.com>
2026-08-29 00:13:02 -0700

    Result screen progress

--- src/main.js
+++ src/main.js
@@ -45,7 +45,7 @@ await video.play();
 
 
 video.style.borderRadius='16px';
-text[2].textContent ='Reading your signal...';
+setTimeout(() => { text[2].style.display = 'none'; result.style.display = 'block'; result.textContent = 'Check complete.'; stream.getTracks().forEach(track => track.stop()); video.style.display = 'none'; }, 30000);
 setTimeout(() => { text[2].textContent = 'Check complete.'; stream.getTracks().forEach(track => track.stop()); video.style.display = 'none'; }, 30000);
 
   } catch (error) {
