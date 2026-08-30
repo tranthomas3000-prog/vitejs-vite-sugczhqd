70a51daf49d0abd87532f01d1cdc97008fe7b791
tranthomas3000-prog <tranthomas3000@gmail.com>
2026-08-29 19:28:29 -0700

    Add sampling timer

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const result=document.querySelector('#result');
 const canvas = document.querySelector('#cameraCanvas');
 const ctx = canvas.getContext('2d');
 const samples=[];
+let samplingInternal;
 result.style.display='none';
 fingerTitle.style.display = 'none';
 text[1].style.display = 'none';
